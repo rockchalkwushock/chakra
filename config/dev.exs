14aac1db604cc58408b1c9ef1d452b52e5595f03
Cody Brunner <rockchalkwushock@icloud.com>
2020-07-25 08:08:40 -0700

    updates dev & test configs, setups db.

--- config/dev.exs
+++ config/dev.exs
@@ -1,14 +1,5 @@
 use Mix.Config
 
-# Configure your database
-config :chakra, Chakra.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "chakra_dev",
-  hostname: "localhost",
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
-
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
@@ -55,3 +46,5 @@ config :phoenix, :stacktrace_depth, 20
 
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
+
+import_config "dev.secret.exs"
--- config/test.exs
+++ config/test.exs
@@ -1,17 +1,5 @@
 use Mix.Config
 
-# Configure your database
-#
-# The MIX_TEST_PARTITION environment variable can be used
-# to provide built-in test partitioning in CI environment.
-# Run `mix help test` for more information.
-config :chakra, Chakra.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "chakra_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
-
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :chakra, ChakraWeb.Endpoint,
@@ -20,3 +8,5 @@ config :chakra, ChakraWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+import_config "test.secret.exs"
