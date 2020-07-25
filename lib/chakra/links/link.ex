63726bc094c84aadc3c0a54314139341d3e5bde6
Cody Brunner <rockchalkwushock@icloud.com>
2020-07-25 09:29:09 -0700

    generates Links related files.

---
+++ lib/chakra/links/link.ex
@@ -0,0 +1,18 @@
+defmodule Chakra.Links.Link do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "links" do
+    field :url, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(link, attrs) do
+    link
+    |> cast(attrs, [:url])
+    |> validate_required([:url])
+    |> unique_constraint(:url)
+  end
+end
---
+++ lib/chakra_web/views/link_view.ex
@@ -0,0 +1,17 @@
+defmodule ChakraWeb.LinkView do
+  use ChakraWeb, :view
+  alias ChakraWeb.LinkView
+
+  def render("index.json", %{links: links}) do
+    %{data: render_many(links, LinkView, "link.json")}
+  end
+
+  def render("show.json", %{link: link}) do
+    %{data: render_one(link, LinkView, "link.json")}
+  end
+
+  def render("link.json", %{link: link}) do
+    %{id: link.id,
+      url: link.url}
+  end
+end
