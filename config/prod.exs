dc883f41a40b6cb7a26956923bebb6b040454519
Cody Brunner <rockchalkwushock@icloud.com>
2020-07-26 07:25:42 -0700

    tweaks config for another deployment.

--- config/prod.exs
+++ config/prod.exs
@@ -1,24 +1,18 @@
 import Config
 
-app_name = System.get_env("APP_NAME")
-database_url = System.get_env("DATABASE_URL")
-pool_size = System.get_env("POOL_SIZE")
-port = System.get_env("PORT")
-secret_key_base = System.get_env("SECRET_KEY_BASE")
-
 config :chakra, Chakra.Repo,
   adapter: Ecto.Adapters.Postgres,
   # Set by Gigalixir
   database: "",
-  pool_size: String.to_integer(port || "10"),
+  pool_size: 2,
   ssl: true,
-  url: database_url
+  url: System.get_env("DATABASE_URL")
 
 config :chakra, ChakraWeb.Endpoint,
-  http: [port: {:system, "PORT"}],
+  http: [port: 4000],
   load_from_system_env: true,
-  secret_key_base: secret_key_base,
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   server: true,
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443]
+  url: [host: "#{app_name}.gigalixirapp.com", port: 443]
 
 config :logger, level: :info
