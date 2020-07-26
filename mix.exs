bee7bbbfe8c9a8886784c8fb9403199ae6755733
Cody Brunner <rockchalkwushock@icloud.com>
2020-07-26 07:10:41 -0700

    bumps version of elixir.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Chakra.MixProject do
     [
       app: :chakra,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.10.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
